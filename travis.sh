d29c18203cde11be654d08a2b9d7b7aa67332d1e
Sergey Nebolsin <sergey@nebols.in>
2016-06-11 23:56:30 +0300

    Update CMake configuration

--- travis.sh
+++ travis.sh
@@ -1,23 +1,10 @@
 #!/bin/sh -x
 # Continuous integration script for Travis
 
-# Build the library and install it.
 echo "## Building and installing libs2..."
-cd geometry
-cmake .
+cmake -Ds2_build_testing=ON -Ds2_build_python=ON geometry
 make -j3
 sudo make install
-
-if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
-	sudo ldconfig -v | grep libs2
-fi
-
-# Build and run the C++ tests
-echo "## Building and running the C++ tests..."
-cd tests
-cmake .
-make -j3
-./tests || exit 1
 
 if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
 	# We really want to use the system version of Python.  Travis'
@@ -25,17 +12,7 @@ if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
 	PATH="/usr/bin:${PATH}"
 	which python2.7
 	python2.7 -V
+	python2.7 -v -c 'import s2'
 fi
 
-# Build and install the Python bindings
-echo "## Building and installing the Python bindings..."
-cd ../python
-cmake .
-make VERBOSE=1
-sudo make install
-
-# Run the Python tests
-echo "## Running the Python tests..."
-python2.7 -v -c 'import s2'
-python2.7 test.py || exit 1
-
+ctest -V
