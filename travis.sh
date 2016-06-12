1e99c2217fc6e8e84afb1b929f54702644b0a95a
Sergey Nebolsin <sergey@nebols.in>
2016-06-12 05:47:12 +0300

    Disable Python build on Travis

--- travis.sh
+++ travis.sh
@@ -4,7 +4,7 @@ set -e
 
 # Continuous integration script for Travis
 echo "## Building and installing libs2..."
-cmake -Ds2_build_testing=ON -Ds2_build_python=ON geometry
+cmake -Ds2_build_testing=ON geometry
 make -j3
 sudo make install
 ctest -V
