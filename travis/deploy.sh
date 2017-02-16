ac681a6a5c3849dfe78725e396d8cd23d16e4f55
David Christenson <david@the-david.com>
2017-02-16 07:14:35 -0700

    Fix

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 macbuild() {
-	pip3 -U install twine setuptools wheel
+	pip3 install -U install twine setuptools wheel
 	rm -rf dist build
 	if [[ "$1" = "sdist" ]]; then
 		python3 setup.py sdist bdist_wheel
@@ -12,7 +12,7 @@ macbuild() {
 	fi
 	python3 setup.py install
 	python3 test.py
-	twine upload --config-file .pypirc -r pypi dist/*.whl dist/*.tar.gz
+	twine upload --skip-existing --config-file .pypirc -r pypi dist/*.whl dist/*.tar.gz
 }
 
 openssl aes-256-cbc -K "$encrypted_dc7bbf7cef27_key" -iv "$encrypted_dc7bbf7cef27_iv" -in travis/secrets.enc -out secrets.tar -d
@@ -20,7 +20,7 @@ tar -xf secrets.tar
 
 if [[ "$DOCKER_IMAGE" ]]; then
 	pip3 install -U twine
-	twine upload --config-file .pypirc -r pypi wheelhouse/*.whl
+	twine upload --skip-existing --config-file .pypirc -r pypi wheelhouse/*.whl
 	echo "Successfully uploaded Linux wheels."
 else
 	curl -L 'https://github.com/Noctem/pogeo-toolchain/releases/download/1.0/macos-openssl-static.tar.xz' -o openssl-static.tar.xz
@@ -36,7 +36,7 @@ else
 	cd ..
 	echo "Successfully installed Python 3.5."
 
-	macbuild
 	pip3 install -U twine
+	macbuild
 	echo "Successfully uploaded Python 3.5 wheel."
 fi
