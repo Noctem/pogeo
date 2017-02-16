b47407c5ea7df9fe01ffff049bf937ba19df644e
David Christenson <david@the-david.com>
2017-02-16 06:44:22 -0700

    Remove signing

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -12,10 +12,7 @@ macbuild() {
 	fi
 	python3 setup.py install
 	python3 test.py
-	for FILE in dist/*.whl dist/*.tar.gz; do
-		gpg2 --batch --passphrase "$GPG_PASSPHRASE" -u 9095FC64 --yes --no-tty -o "${FILE}.asc" --sign "$FILE" || TRUE
-	done
-	twine upload --config-file .pypirc -r pypi dist/*.whl dist/*.asc dist/*.tar.gz
+	twine upload --config-file .pypirc -r pypi dist/*.whl dist/*.tar.gz
 }
 
 openssl aes-256-cbc -K "$encrypted_dc7bbf7cef27_key" -iv "$encrypted_dc7bbf7cef27_iv" -in travis/secrets.enc -out secrets.tar -d
@@ -23,16 +20,9 @@ tar -xf secrets.tar
 
 if [[ "$DOCKER_IMAGE" ]]; then
 	pip3 install -U twine
-	gpg2 --fast-import signing.asc
-	for FILE in wheelhouse/*.whl; do
-		gpg2 --batch --passphrase "$GPG_PASSPHRASE" -u 9095FC64 --yes --no-tty -o "${FILE}.asc" --sign "$FILE" || TRUE
-	done
-	twine upload --config-file .pypirc -r pypi wheelhouse/*.whl wheelhouse/*.asc
+	twine upload --config-file .pypirc -r pypi wheelhouse/*.whl
 	echo "Successfully uploaded Linux wheels."
 else
-	brew install gnupg2
-	gpg2 --fast-import signing.asc
-
 	curl -L 'https://github.com/Noctem/pogeo-toolchain/releases/download/1.0/macos-openssl-static.tar.xz' -o openssl-static.tar.xz
 	tar -xf openssl-static.tar.xz
 	export OPENSSL_ROOT_DIR="$(pwd)/openssl-static"
