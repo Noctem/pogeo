de556296a181cd64af44939b306551aa851309aa
David Christenson <mail@noctem.xyz>
2017-02-18 22:36:17 -0700

    Add 32-bit build/deployment, add distance tests

--- travis/manylinux-build.sh
+++ travis/manylinux-build.sh
@@ -2,14 +2,22 @@
 
 set -e -x
 
-curl -L 'https://github.com/Noctem/pogeo-toolchain/releases/download/1.0/centos5-toolchain.tar.bz2' -o toolchain.tar.bz2
+if [[ "$(uname -m)" = i686 ]]; then
+	TOOLCHAIN_URL='https://github.com/Noctem/pogeo-toolchain/releases/download/1.0/centos5-toolchain-i686.tar.bz2'
+	export LD_LIBRARY_PATH="/toolchain/lib:${LD_LIBRARY_PATH}"
+	MFLAG="-m32"
+else
+	TOOLCHAIN_URL='https://github.com/Noctem/pogeo-toolchain/releases/download/1.0/centos5-toolchain.tar.bz2'
+	export LD_LIBRARY_PATH="/toolchain/lib64:/toolchain/lib:${LD_LIBRARY_PATH}"
+	MFLAG="-m64"
+fi
 
+curl -L "$TOOLCHAIN_URL" -o toolchain.tar.bz2
 tar -C / -xf toolchain.tar.bz2
 
 export PATH="/toolchain/bin:${PATH}"
-export LD_LIBRARY_PATH="/toolchain/lib64:/toolchain/lib:${LD_LIBRARY_PATH}"
-export CFLAGS="-I/toolchain/include -static-libgcc -static-libstdc++"
-export CXXFLAGS="-I/toolchain/include -static-libgcc -static-libstdc++"
+export CFLAGS="-I/toolchain/include -static-libgcc -static-libstdc++ ${MFLAG}"
+export CXXFLAGS="-I/toolchain/include -static-libgcc -static-libstdc++ ${MFLAG}"
 export OPENSSL_ROOT_DIR="/toolchain"
 
 # Compile wheels
