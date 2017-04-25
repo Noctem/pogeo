09a3c5f4567d4c8864f89c4c8f4a3c259a45d051
David Christenson <mail@noctem.xyz>
2017-04-24 18:33:47 -0700

    Support pickling CellCache, simplify array casting

--- pogeo/cellcache.pyx
+++ pogeo/cellcache.pyx
@@ -6,9 +6,9 @@ from libc.string cimport memcpy
 from libcpp.vector cimport vector
 from libcpp.unordered_map cimport unordered_map
 
-from cython.operator cimport dereference as deref
+from cython.operator cimport postincrement as incr, dereference as deref
 
-from .array cimport array, clone
+from .array cimport array
 from .const cimport AXIS_HEIGHT
 from .geo.s2 cimport S2Point
 from .geo.s2cap cimport S2Cap
@@ -18,13 +18,29 @@ from .types cimport vector_uint64
 
 DEF S2_LEVEL = 15
 
-cdef array ARRAY_TEMPLATE = array('Q', [])
-
 
 cdef class CellCache:
     def __cinit__(self):
         self.coverer.set_min_level(S2_LEVEL)
         self.coverer.set_max_level(S2_LEVEL)
+
+    def __getstate__(self):
+        cdef vector_uint64 cells
+        cdef uint64_t cell
+        cdef dict state = {}
+        it = self.cache.begin()
+        while it != self.cache.end():
+            cell = deref(it).first
+            cells = deref(it).second
+            state[cell] = <array>cells
+            incr(it)
+        return state
+
+    def __setstate__(self, dict state):
+        cdef uint64_t cell
+        cdef vector_uint64 cells
+        for cell, cells in state.items():
+            self.cache[cell] = <vector_uint64>cells
 
     def get_cell_ids(self, Location p):
         cdef size_t size
@@ -34,19 +50,13 @@ cdef class CellCache:
 
         it = self.cache.find(cell)
         if it != self.cache.end():
-            size = deref(it).second.size()
-            cell_array = clone(ARRAY_TEMPLATE, size)
-            memcpy(&cell_array.data.as_ulonglongs[0], &deref(it).second, size * 8)
-            return cell_array
+            return <array>deref(it).second
 
         cdef S2Cap cap = S2Cap.FromAxisHeight(point, AXIS_HEIGHT)
 
         cdef vector_uint64 cells
         self.coverer.GetCellIds(cap, &cells)
 
-        size = cells.size()
-        cell_array = clone(ARRAY_TEMPLATE, size)
-        memcpy(&cell_array.data.as_ulonglongs[0], &cells[0], size * 8)
         self.cache[cell] = cells
 
-        return cell_array
+        return <array>cells
