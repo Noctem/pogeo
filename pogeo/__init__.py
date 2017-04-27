a1ccb9dfa4e3f87c34417090929666aaa0778a76
David Christenson <mail@noctem.xyz>
2017-04-27 14:04:33 -0600

    Add Rectangle class

--- pogeo/__init__.py
+++ pogeo/__init__.py
@@ -4,7 +4,9 @@ __author__ = 'David Christenson'
 __license__ = 'Apache License'
 __copyright__ = 'Copyright (c) 2017 David Christenson <https://github.com/Noctem>'
 
+from .cellcache import CellCache
 from .location import Location
 from .loop import Loop
 from .polygon import Polygon
+from .rectangle import Rectangle
 from .utils import get_bearing, get_distance, get_distance_meters, get_cell_ids
---
+++ pogeo/rectangle.pyx
@@ -0,0 +1,64 @@
+from libc.math cimport pow
+
+from .const cimport EARTH_RADIUS_KILOMETERS
+from .cpython cimport _Py_HashDouble, Py_hash_t, Py_uhash_t
+from .geo.s2 cimport S2, S2Point
+from .geo.s2latlng cimport S2LatLng
+from .geo.s2latlngrect cimport S2LatLngRect
+from .location cimport Location
+
+
+cdef class Rectangle:
+    def __cinit__(self, tuple point1, tuple point2, bint bound=True):
+        cdef:
+            double lat1, lat2, lon1, lon2
+            S2LatLng lo, hi
+        lat1, lon1 = point1
+        lat2, lon2 = point2
+        if lat1 > lat2:
+            self.north = lat1
+            self.south = lat2
+        else:
+            self.south = lat1
+            self.north = lat2
+        if lon1 > lon2:
+            self.east = lon1
+            self.west = lon2
+        else:
+            self.west = lon1
+            self.east = lon2
+        lo = S2LatLng.FromDegrees(self.south, self.west)
+        hi = S2LatLng.FromDegrees(self.north, self.east)
+        self.latlngrect = S2LatLngRect(lo, hi)
+        self.unbound = not bound
+
+    def __contains__(self, Location loc):
+        return self.unbound or (
+            loc.latitude >= self.south
+            and loc.latitude <= self.north
+            and loc.longitude >= self.west
+            and loc.longitude <= self.east)
+
+    def __hash__(self):
+        cdef Py_uhash_t mult = 1000003
+        cdef Py_uhash_t x = 0x345678
+        cdef Py_hash_t y
+
+        cdef double[4] bounds = [self.south, self.east, self.north, self.west]
+        cdef double bound
+        for bound in bounds:
+            y = _Py_HashDouble(bound)
+            x = (x ^ y) * mult
+            mult += <Py_hash_t>(82520 + 8)
+        return x + 97531
+
+    @property
+    def bounds(self):
+        return self.south, self.east, self.north, self.west
+
+    @property
+    def area(self):
+        """Returns the square kilometers for configured area"""
+        return self.latlngrect.Area() * pow(EARTH_RADIUS_KILOMETERS, 2)
+
+
