f7c56ce497a0bc426d708a8628fdceb63f9b8c45
David Christenson <mail@noctem.xyz>
2017-05-12 14:42:01 -0600

    Build all with c++14

--- lib/vectorutils.cpp
+++ lib/vectorutils.cpp
@@ -11,13 +11,13 @@ using json11::Json;
 string dump_after_id(const Json::array &arr, int id) {
   Json::array jarray;
   copy_if(arr.cbegin(), arr.cend(), jarray.begin(),
-          [&id](const Json &object) { return object[string("id")] > id; });
+          [id](const Json &object) { return object[string("id")] > id; });
   return Json(jarray).dump();
 }
 
 void remove_expired(Json::array &arr, int now) {
   arr.erase(std::remove_if(arr.begin(), arr.end(),
-                           [&now](const Json &object) {
+                           [now](const Json &object) {
                              return object[string("expire")] < now;
                            }),
             arr.end());
